55be25ca4586bc7e4b8d970a637911c67b1c9c1c
Paulin (Pasha) Majorovskaia <majorovpaul@gmail.com>
2023-07-27 17:28:41 +0300

    page new code

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -2,7 +2,17 @@ import React from "react";
 import classes from "./Page.module.css";
 
 function Page() {
-  return <div>Yeey!</div>;
+  return (
+    <div className={classes.app}>
+      <div className={classes.origin}>
+        <div className={classes.picture}>
+          <ul>
+            <li>Bikes</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
