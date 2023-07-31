d7d110a99cd274440b8291e0b28a3d7a95809de7
Paulin (Pasha) Majorovskaia <majorovpaul@gmail.com>
2023-07-31 18:00:46 +0300

    page update new css

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -4,13 +4,36 @@ import classes from "./Page.module.css";
 function Page() {
   return (
     <div className={classes.app}>
-      <div className={classes.origin}>
-        <div className={classes.picture}>
+      <header>
+        <h1>Rent our bicycles!</h1>
+        <nav>
           <ul>
-            <li>Bikes</li>
+            <li>
+              <a href="#">Sign in</a>
+            </li>
+            <li>
+              <a href="#">Sign up</a>
+            </li>
           </ul>
-        </div>
-      </div>
+        </nav>
+      </header>
+      <div className={classes.picture}></div>
+      <main className={classes.main}>
+        <p>Our bicycles are the best!</p>
+      </main>
+      <footer>
+        <nav>
+          <h2>Contacts</h2>
+          <ul>
+            <li>
+              <a href="#">some@email</a>
+            </li>
+            <li>
+              <a>some adress</a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
     </div>
   );
 }
