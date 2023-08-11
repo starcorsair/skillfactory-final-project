f9573eb42858f7a5d62c01497fc957470f085b54
Paulin (Pasha) Majorovskaia <majorovpaul@gmail.com>
2023-08-11 18:13:53 +0300

    new edit

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -6,49 +6,50 @@ import classes from "./Page.module.css";
 
 function Page() {
   return (
-    <div className={classes.app}>
-      <header>
-        <h1>Прокатитесь на наших велосипедах!</h1>
-        <nav className={classes.nav_panel}>
-          <ul className={classes.list_page}>
-            <li>
-              <Link to="/registration" className={classes.link_page}>
-                Регистрация
-              </Link>
-            </li>
-            <li>
-              <Link to="/login" className={classes.link_page}>
-                Войти
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <div className={classes.picture}></div>
-      <main className={classes.main}>
-        <p className={classes.text_main}>
-          Ищете способ провести время на свежем воздухе? Тогда прокат
-          велосипедов - это то, что вам нужно! Наши велосипеды в отличном
-          состоянии, а наши цены доступны каждому.
-        </p>
-        <p className={classes.text_main}>
-          Не упустите возможность прокатиться на наших велосипедах! Забронируйте
-          свой велосипед уже сегодня и получите незабываемые впечатления от
-          катания на свежем воздухе
-        </p>
-        <div className={classes.forms_main}>
-          <h3 className={classes.head_text}>Произошла кража велосипеда?</h3>
-          <Link to="/report" className={classes.link_main}>
-            Напишите нам:
-          </Link>
+    <div className={classes.wrap}>
+      <div className={classes.app}>
+        <header>
+          <h1>Прокатитесь на наших велосипедах!</h1>
+          <nav className={classes.nav_panel}>
+            <ul className={classes.list_page}>
+              <li>
+                <Link to="/registration" className={classes.link_page}>
+                  Регистрация
+                </Link>
+              </li>
+              <li>
+                <Link to="/login" className={classes.link_page}>
+                  Войти
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <div className={classes.picture}></div>
+        <main className={classes.main}>
+          <p className={classes.text_main}>
+            Ищете способ провести время на свежем воздухе? Тогда прокат
+            велосипедов - это то, что вам нужно! Наши велосипеды в отличном
+            состоянии, а наши цены доступны каждому.
+          </p>
+          <p className={classes.text_main}>
+            Не упустите возможность прокатиться на наших велосипедах!
+            Забронируйте свой велосипед уже сегодня и получите незабываемые
+            впечатления от катания на свежем воздухе
+          </p>
+          <div className={classes.forms_main}>
+            <h3 className={classes.head_text}>Произошла кража велосипеда?</h3>
+            <Link to="/report" className={classes.link_main}>
+              Напишите нам:
+            </Link>
 
-          <h3 className={classes.head_text}>Хотите присоедениться к нам?</h3>
-          <Link to="/registration" className={classes.link_main}>
-            Зарегистрируйтесь!
-          </Link>
-        </div>
-      </main>
-
+            <h3 className={classes.head_text}>Хотите присоединиться к нам?</h3>
+            <Link to="/registration" className={classes.link_main}>
+              Зарегистрируйтесь!
+            </Link>
+          </div>
+        </main>
+      </div>
       <Officers />
       <Reports />
     </div>
